be2f017c1c7e9402a5243be1e3234ed08da93439
Bram Gerritsen <bgerritsen@emico.nl>
2019-05-22 13:30:38 +0200

    First module version for public release 1.0.0

--- src/Setup/UpgradeSchema.php
+++ src/Setup/UpgradeSchema.php
@@ -49,7 +49,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
         if (version_compare($context->getVersion(), '0.4.2') < 0) {
             $this->addCanonicalUrlColumn($setup);
         }
-        if (version_compare($context->getVersion(), '1.0.1') < 0) {
+        if (version_compare($context->getVersion(), '1.0.0') < 0) {
             $this->addHideSelectedFiltersColumn($setup);
         }
         $setup->endSetup();
