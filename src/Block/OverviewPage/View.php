6412fd21df74db7d3ad50d9d308a29fe36a7d44c
Bram Gerritsen <bgerritsen@emico.nl>
2022-02-03 17:08:38 +0100

    Hyva default template calls prepareLayout for each Esi block to retrieve cache tags. This caused an error on the overviewpage

--- src/Block/OverviewPage/View.php
+++ src/Block/OverviewPage/View.php
@@ -12,7 +12,9 @@ use Emico\AttributeLanding\Api\Data\OverviewPageInterface;
 use Emico\AttributeLanding\Api\LandingPageRepositoryInterface;
 use Emico\AttributeLanding\Model\LandingPageContext;
 use Emico\AttributeLanding\Model\Page\ImageUploader;
+use Magento\Theme\Block\Html\Breadcrumbs;
 use Magento\Cms\Model\Template\FilterProvider;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Template;
 use Psr\Log\LoggerInterface;
@@ -94,7 +96,7 @@ class View extends Template
      * @param LandingPageInterface $landingPage
      * @return string|null
      */
-    public function getLandingPageImage(LandingPageInterface $landingPage)
+    public function getLandingPageImage(LandingPageInterface $landingPage): ?string
     {
         $image = $landingPage->getOverviewPageImage();
         if ($image === null) {
@@ -137,30 +139,40 @@ class View extends Template
     /**
      * Preparing layout
      *
-     * @return \Magento\Catalog\Block\Breadcrumbs
+     * @return View
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
      */
-    protected function _prepareLayout()
+    protected function _prepareLayout(): View
     {
-        if ($breadcrumbsBlock = $this->getLayout()->getBlock('breadcrumbs')) {
-            $breadcrumbsBlock->addCrumb(
-                'home',
-                [
-                    'label' => __('Home'),
-                    'title' => __('Go to Home Page'),
-                    'link' => $this->_storeManager->getStore()->getBaseUrl()
-                ]
-            );
-
-            $overviewPage = $this->getOverviewPage();
-            $breadcrumbsBlock->addCrumb(
-                'overviewpage',
-                [
-                    'label' => __($overviewPage->getName()),
-                    'title' => __($overviewPage->getName()),
-                    'link' => $overviewPage->getUrlPath()
-                ]
-            );
+        if ($this->landingPageContext->getOverviewPage() === null) {
+            return parent::_prepareLayout();
         }
+
+        $breadcrumbsBlock = $this->getLayout()->getBlock('breadcrumbs');
+        if (!$breadcrumbsBlock instanceof Breadcrumbs) {
+            return parent::_prepareLayout();
+        }
+
+        $breadcrumbsBlock->addCrumb(
+            'home',
+            [
+                'label' => __('Home'),
+                'title' => __('Go to Home Page'),
+                'link' => $this->_storeManager->getStore()->getBaseUrl()
+            ]
+        );
+
+        $overviewPage = $this->getOverviewPage();
+        $breadcrumbsBlock->addCrumb(
+            'overviewpage',
+            [
+                'label' => __($overviewPage->getName()),
+                'title' => __($overviewPage->getName()),
+                'link' => $overviewPage->getUrlPath()
+            ]
+        );
+
         return parent::_prepareLayout();
     }
 }
