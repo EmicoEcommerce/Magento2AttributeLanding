9a4e772ed45fdb81a08e676ef24213de3c0ef13b
ah-net <103565001+ah-net@users.noreply.github.com>
2023-07-07 15:16:50 +0200

    Wip

---
+++ src/view/adminhtml/web/js/form/element/filter-attributes.js
@@ -0,0 +1,65 @@
+define([
+    'Magento_Ui/js/form/element/ui-select',
+    'jquery'
+], function (Select) {
+    'use strict';
+
+    return Select.extend({
+
+        initialize: function () {
+            this._super();
+            this.onUpdate(this.value());
+            return this;
+        },
+
+        /**
+         * Parse data and set it to options.
+         *
+         * @param {Object} data - Response data object.
+         * @returns {Object}
+         */
+        setParsed: function (data) {
+            var option = this.parseData(data);
+
+            if (data.error) {
+                return this;
+            }
+
+            this.options([]);
+            this.setOption(option);
+            this.set('newOption', option);
+        },
+
+        /**
+         * Normalize option object.
+         *
+         * @param {Object} data - Option object.
+         * @returns {Object}
+         */
+        parseData: function (data) {
+            return {
+                'is_active': data.category['is_active'],
+                level: data.category.level,
+                value: data.category['entity_id'],
+                label: data.category.name,
+                parent: data.category.parent
+            };
+        },
+
+        /**
+         * Change currently selected option
+         *
+         * @param {String} id
+         */
+        onUpdate: function(value){
+            setTimeout(function() {
+                if((value == '')||(value == undefined)) {
+                    document.querySelector('.filter_attributes').hide();
+
+                } else {
+                    document.querySelector('.filter_attributes').show();
+                }
+            }, 1000);
+        },
+    });
+});
