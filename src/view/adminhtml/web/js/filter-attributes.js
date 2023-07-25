290f11b5eaf829425ce38cf516b2af0c14e063cd
ah-net <103565001+ah-net@users.noreply.github.com>
2023-07-25 09:41:17 +0200

    Wip

--- src/view/adminhtml/web/js/filter-attributes.js
+++ src/view/adminhtml/web/js/filter-attributes.js
@@ -1,40 +1,61 @@
 define([
     'jquery',
     'uiRegistry',
-    'mage/url'
+    'mage/url',
 ], function($, registery, url) {
     'use strict';
 
-    $.widget('mage.tweakwise_filter_attributes', {
+    $.widget('tweakwise.filter_attributes', {
         /**
          * Bind handlers to events
          */
-        _create: function() {
+        _create: function(config, element) {
+            this._updateAttributes(this.options.name);
+        },
+
+        _updateAttributes: function (name) {
             var category_id = registery.get('emico_attributelanding_page_form.emico_attributelanding_page_form.general.category_id').value();
-            var element = this.element;
+            var element =  $('select[name="' + name + '"]');
+            var selectedValue = element.val();
 
             var facetUrl = url.build('/tweakwise/ajax/facets/category/' + category_id);
             $.getJSON(facetUrl, function( data ) {
                 element.empty();
                 data.data.forEach(value => {
-                    element.append($("<option></option>").attr("value", value.value).text(value.label));
+                    if(value.value != selectedValue) {
+                        element.append($("<option></option>").attr("value", value.value).text(value.label));
+                    } else {
+                        element.append($("<option></option>").attr("value", value.value).text(value.label).attr("selected", "selected"));
+                    }
+
                 });
             });
 
-            element.on('change', this.updateAttributeValues());
+            this._bindEvents(name);
         },
 
-        updateAttributeValues: function () {
-            var element = this.element;
-            var facetValue = element.val();
-            var category_id = registery.get('emico_attributelanding_page_form.emico_attributelanding_page_form.general.category_id').value();
+        _bindEvents(name) {
+            $('select[name="' + name + '"]').on('change', function(evt) {
+                var name = evt.target.name;
+                var facetValue = evt.target.value;
+                var category_id = registery.get('emico_attributelanding_page_form.emico_attributelanding_page_form.general.category_id').value();
+                var select = $('select[name="' + name.replace('[attribute]', '[value]') + '"]');
+                var selectedValue = select.val();
 
-            var facetUrl = url.build('/tweakwise/ajax/facetattributes/category/' + category_id + '/facetkey/' + facetValue);
-            $.getJSON(facetUrl, function( data ) {
-                console.log(data);
+                console.log(selectedValue);
+
+                var facetUrl = url.build('/tweakwise/ajax/facetattributes/category/' + category_id + '/facetkey/' + facetValue);
+                $.getJSON(facetUrl, function( data ) {
+                    select.empty();
+                    data.data.forEach(value => {
+                        select.append($("<option></option>").attr("value", value.value).text(value.label));
+                    });
+                });
             });
+
+            $('select[name="' + name + '"]').trigger('change');
         }
     });
 
-    return $.mage.tweakwise_filter_attributes;
+    return $.tweakwise.filter_attributes;
 });
