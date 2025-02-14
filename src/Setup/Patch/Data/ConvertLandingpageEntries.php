b69305d32538c56e93139fedb9a94640bb3a111c
ah-net <103565001+ah-net@users.noreply.github.com>
2025-02-14 13:48:48 +0100

    Update ConvertLandingpageEntries.php

--- src/Setup/Patch/Data/ConvertLandingpageEntries.php
+++ src/Setup/Patch/Data/ConvertLandingpageEntries.php
@@ -2,6 +2,7 @@
 
 namespace Emico\AttributeLanding\Setup\Patch\Data;
 
+use Emico\AttributeLanding\Api\Data\LandingPageInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Store\Model\StoreManagerInterface;
@@ -53,12 +54,25 @@ class ConvertLandingpageEntries implements DataPatchInterface
         ];
 
         $fields = [
-            'active', 'url_path', 'category_id', 'heading', 'header_image', 'meta_title',
-            'meta_keywords', 'meta_description', 'content_first', 'content_last',
-            'filter_attributes', 'tweakwise_filter_template', 'is_filter_link_allowed',
-            'canonical_url', 'hide_selected_filters', 'tweakwise_sort_template',
-            'tweakwise_builder_template'
+            LandingPageInterface::ACTIVE,
+            LandingPageInterface::URL_PATH,
+            LandingPageInterface::CATEGORY_ID,
+            LandingPageInterface::HEADING,
+            LandingPageInterface::HEADER_IMAGE,
+            LandingPageInterface::META_TITLE,
+            LandingPageInterface::META_KEYWORDS,
+            LandingPageInterface::META_DESCRIPTION,
+            LandingPageInterface::CONTENT_FIRST,
+            LandingPageInterface::CONTENT_LAST,
+            LandingPageInterface::FILTER_ATTRIBUTES,
+            LandingPageInterface::TWEAKWISE_FILTER_TEMPLATE,
+            LandingPageInterface::IS_FILTER_LINK_ALLOWED,
+            LandingPageInterface::CANONICAL_URL,
+            LandingPageInterface::HIDE_SELECTED_FILTERS,
+            LandingPageInterface::TWEAKWISE_SORT_TEMPLATE,
+            LandingPageInterface::TWEAKWISE_BUILDER_TEMPLATE
         ];
+
 
         foreach ($fields as $field) {
             if (isset($landingPage[$field])) {
